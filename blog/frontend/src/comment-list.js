b9d1508678e8d6ff7e59b09c1268e67815bf1f1c
Lucas Bittencurt <bittencurt.lucas@protonmail.com>
2021-06-09 08:21:35 -0300

    class 48

--- blog/frontend/src/comment-list.js
+++ blog/frontend/src/comment-list.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 const CommentList = ({ comments }) => {
 
+    const handleCommentStatus = ({ status, content }) => {
+        if (status === 'approved') {
+            return content
+        }
+        if (status === 'pending') {
+            return 'This comment is awaiting moderation.'
+        }
+        if (status === 'rejected') {
+            return 'This comment has been rejected'
+        }
+    } 
+
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+        return <li key={comment.id}>{handleCommentStatus(comment)}</li>
     })
 
     return <ul>{renderedComments}</ul>
