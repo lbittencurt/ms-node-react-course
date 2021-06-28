cca7c1774ec339698c2903d688ca1c021c05b931
Lucas Bittencurt <bittencurt.lucas@protonmail.com>
2021-06-27 23:32:57 -0300

    section 7 and 8

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,13 +1,42 @@
 import express from 'express'
+import 'express-async-errors'
+import mongoose from 'mongoose'
+
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
+import { errorHandler } from './middlewares/error-handler'
+import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
-app.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there.')
-})
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!')
-})
+app.all('*', () => { throw new NotFoundError() })
+
+app.use(errorHandler)
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        console.log('Connected to mongodb.')
+    } catch (error) {
+        console.log(error)
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!')
+    })
+}
+
+start()
